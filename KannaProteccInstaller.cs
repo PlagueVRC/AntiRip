0e17a33ea832991bfc07928af76ee35344b593a3
Mistress Plague <krewellamusicps3@gmail.com>
2023-12-17 13:54:32 +0000

    Refactoring

--- KannaProteccInstaller.cs
+++ KannaProteccInstaller.cs
@@ -1,12 +1,8 @@
 #if UNITY_EDITOR && UNITY_2022
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
-using System.Linq;
 using System.Net;
-using System.Net.Http;
 using Newtonsoft.Json;
 using UnityEditor;
 using UnityEngine;
@@ -15,7 +11,7 @@ using UnityEngine;
 public class KannaProteccInstaller
 {
     [InitializeOnLoadMethod]
-    static void CheckForInstall()
+    private static void CheckForInstall()
     {
         if (Environment.UserName == "krewe") // Me
         {
@@ -30,7 +26,7 @@ public class KannaProteccInstaller
 
         Debug.Log($"Last Commit: {LastCommit.sha}");
 
-        var CommitFile = "Assets/LastKannaProteccCommit.txt";
+        const string CommitFile = "Assets/LastKannaProteccCommit.txt";
 
         if (File.Exists(CommitFile))
         {
@@ -55,7 +51,7 @@ public class KannaProteccInstaller
         }
     }
 
-    static void Install()
+    private static void Install()
     {
         using (var client = new WebClient())
         {
@@ -74,139 +70,14 @@ public class GitHubAPI
     
     public class BranchResponse
     {
-        public string name { get; set; }
         public Commit commit { get; set; }
-        public _links _links { get; set; }
         [JsonProperty("protected")]
-        public bool protecc {
-            get;
-            set;
-        }
-
-        public Protection protection { get; set; }
-        public string protection_url { get; set; }
+        public bool protecc { get; set; }
     }
 
     public class Commit
     {
         public string sha { get; set; }
-        public string node_id { get; set; }
-        public Commit1 commit { get; set; }
-        public string url { get; set; }
-        public string html_url { get; set; }
-        public string comments_url { get; set; }
-        public Author author { get; set; }
-        public Committer committer { get; set; }
-        public Parents[] parents { get; set; }
-    }
-
-    public class Commit1
-    {
-        public Author1 author { get; set; }
-        public Committer1 committer { get; set; }
-        public string message { get; set; }
-        public Tree tree { get; set; }
-        public string url { get; set; }
-        public int comment_count { get; set; }
-        public Verification verification { get; set; }
-    }
-
-    public class Author1
-    {
-        public string name { get; set; }
-        public string email { get; set; }
-        public string date { get; set; }
-    }
-
-    public class Committer1
-    {
-        public string name { get; set; }
-        public string email { get; set; }
-        public string date { get; set; }
-    }
-
-    public class Tree
-    {
-        public string sha { get; set; }
-        public string url { get; set; }
-    }
-
-    public class Verification
-    {
-        public bool verified { get; set; }
-        public string reason { get; set; }
-        public object signature { get; set; }
-        public object payload { get; set; }
-    }
-
-    public class Author
-    {
-        public string login { get; set; }
-        public int id { get; set; }
-        public string node_id { get; set; }
-        public string avatar_url { get; set; }
-        public string gravatar_id { get; set; }
-        public string url { get; set; }
-        public string html_url { get; set; }
-        public string followers_url { get; set; }
-        public string following_url { get; set; }
-        public string gists_url { get; set; }
-        public string starred_url { get; set; }
-        public string subscriptions_url { get; set; }
-        public string organizations_url { get; set; }
-        public string repos_url { get; set; }
-        public string events_url { get; set; }
-        public string received_events_url { get; set; }
-        public string type { get; set; }
-        public bool site_admin { get; set; }
-    }
-
-    public class Committer
-    {
-        public string login { get; set; }
-        public int id { get; set; }
-        public string node_id { get; set; }
-        public string avatar_url { get; set; }
-        public string gravatar_id { get; set; }
-        public string url { get; set; }
-        public string html_url { get; set; }
-        public string followers_url { get; set; }
-        public string following_url { get; set; }
-        public string gists_url { get; set; }
-        public string starred_url { get; set; }
-        public string subscriptions_url { get; set; }
-        public string organizations_url { get; set; }
-        public string repos_url { get; set; }
-        public string events_url { get; set; }
-        public string received_events_url { get; set; }
-        public string type { get; set; }
-        public bool site_admin { get; set; }
-    }
-
-    public class Parents
-    {
-        public string sha { get; set; }
-        public string url { get; set; }
-        public string html_url { get; set; }
-    }
-
-    public class _links
-    {
-        public string self { get; set; }
-        public string html { get; set; }
-    }
-
-    public class Protection
-    {
-        public bool enabled { get; set; }
-        public Required_status_checks required_status_checks { get; set; }
-    }
-
-    public class Required_status_checks
-    {
-        public string enforcement_level { get; set; }
-        public object[] contexts { get; set; }
-        public object[] checks { get; set; }
     }
 
     #endregion
